e3d660b9313b80b565448dc804dd77eeae423011
Guillermo Castaño <gucastano3@poligran.edu.co>
2025-11-12 20:20:09 -0500

    Cambio

--- Front_recursos_humanos/src/App.vue
+++ Front_recursos_humanos/src/App.vue
@@ -36,7 +36,7 @@ onMounted(() => {
 <template>
   <div class="container">
     <h1>Proyecto entrega Integración continua Politécnico Grancolombiano de la clase integracion continua</h1>
-    <h2>Hacemos un cambio menor dentro del código para probar el webhook</h2>
+    <h2>Hacemos un cambio menor dentro   del código para probar el webhook</h2>
     
     <div class="empleados-section">
       <h2>Lista de Empleados</h2>
